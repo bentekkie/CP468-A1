b6695c9ef8f97e0770257291c9cd80c7f838a0bd
Benjamin Segall <bentekkie@gmail.com>
2019-09-12 11:05:06 -0400

    first commit

---
+++ calculatepath.py
@@ -0,0 +1,61 @@
+from typing import Tuple, List, Dict
+from queue import Queue
+n = 3
+end = (True,n,n)
+start = (False,0,0)
+
+def valid(state: tuple):
+    return 0 <= state[1] <= n and 0 <= state[2] <= n and (state[1] == state[2] or state[1] % n is 0)
+        
+
+def neighbours(curr : tuple):
+    d = -1 if curr[0] else 1
+    other = not curr[0] 
+    yield from (state for state in (
+        (other, curr[1]+(1*d), curr[2]),
+        (other, curr[1], curr[2]+(1*d)),
+        (other, curr[1]+(1*d), curr[2]+(1*d)),
+        (other, curr[1]+(2*d), curr[2]),
+        (other, curr[1], curr[2]+(2*d)),
+     ) if valid(state))
+
+adjList = dict()
+def path(curr, prev):
+    while curr is not start:
+        yield curr
+        curr = prev[curr]
+    yield start
+
+def stateToString(state):
+    return f"{n-state[1]}✝{n-state[2]}☺{('~>' if state[0] else '<~')}{state[1]}✝{state[2]}☺"
+    
+
+for i in range(0,n+1):
+    for j in range(0,n+1):
+        states = [(True,i,j),(False,i,j)]
+        for state in states:
+            if valid(state):
+                adjList[state] = [x for x in neighbours(state)]
+
+
+nodesToProcess =  Queue()
+nodesToProcess.put(start)
+visited = set()
+prev = dict()
+while not nodesToProcess.empty():
+    node = nodesToProcess.get()
+    if node in visited:
+        continue
+    visited.add(node)
+    for x in adjList[node]:
+        if x not in prev:
+            prev[x] = node
+        if x not in visited:
+            nodesToProcess.put(x)
+
+correct_path = list(reversed([x for x in path(end,prev)]))
+correct_edges = [tuple(sorted((correct_path[i],correct_path[i+1]))) for i in range(len(correct_path)-1)]
+
+if __name__ == "__main__":
+    for state in correct_path:
+        print(stateToString(state))
---
+++ generategraph.py
@@ -0,0 +1,22 @@
+#%%
+import calculatepath as lib
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+figure(num=None, figsize=(20, 10), dpi=80, facecolor='w', edgecolor='k')
+
+G = nx.Graph()
+G.add_nodes_from(lib.adjList.keys())
+G.add_edges_from( {tuple(sorted((key,value))) for key,values in lib.adjList.items() for value in values})
+
+plt.subplot(111)
+plt.tight_layout(pad=5)
+nx.draw_kamada_kawai(
+    G, 
+    with_labels=True, 
+    font_weight='bold',
+    font_size=20,
+    edgecolors=["#FF0000" if state == lib.start or state == lib.end else '#00FF00' for state in list(G.nodes)],
+    labels={state:lib.stateToString(state) for state in lib.adjList.keys()},
+    node_color=["#00FF00" if state in lib.correct_path else "#00FFFF" for state in list(G.nodes)],
+    edge_color=["#00FF00" if edge in lib.correct_edges else "#000000" for edge in list(G.edges)])
