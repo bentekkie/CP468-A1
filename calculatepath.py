a3f2491bccee712b4ac96833162bbb14102a4436
Benjamin Segall <bentekkie@gmail.com>
2019-09-23 20:18:45 -0400

    added edge labels

--- calculatepath.py
+++ calculatepath.py
@@ -1,46 +1,50 @@
-from typing import Tuple, List, Dict
+from typing import Tuple, List, Dict, Set
 from queue import Queue
 n = 3
-end = (True,n,n)
-start = (False,0,0)
+State = Tuple[bool,int,int]
+end : State = (True,n,n)
+start : State = (False,0,0)
 
-def valid(state: tuple):
+def valid(state: State) -> bool:
     return 0 <= state[1] <= n and 0 <= state[2] <= n and (state[1] == state[2] or state[1] % n is 0)
         
 
-def neighbours(curr : tuple):
+def neighbours(curr : State) -> List[State]:
     d = -1 if curr[0] else 1
     other = not curr[0] 
-    yield from (state for state in (
-        (other, curr[1]+d, curr[2]),
+    return [state for state in 
+        ((other, curr[1]+d, curr[2]),
         (other, curr[1], curr[2]+d),
         (other, curr[1]+d, curr[2]+d),
         (other, curr[1]+(2*d), curr[2]),
-        (other, curr[1], curr[2]+(2*d)),
-     ) if valid(state))
+        (other, curr[1], curr[2]+(2*d)))
+        if valid(state)]
 
-adjList = dict()
-def path(curr, prev):
+adjList :  Dict[State,List[State]] = dict()
+def path(curr : State, prev : Dict[State,State]):
     while curr is not start:
         yield curr
         curr = prev[curr]
     yield start
 
-def stateToString(state):
-    return f"{n-state[1]}✝{n-state[2]}☺{('~>' if state[0] else '<~')}{state[1]}✝{state[2]}☺"
-    
-adjList = {state:list(neighbours(state)) for i in range(0,n+1) for j in range(0,n+1) for state in ((True,i,j),(False,i,j)) if valid(state)}
+def stateToString(state : State):
+    return f"{n-state[1]}✝{n-state[2]}☺{('~V' if state[0] else 'V~')}{state[1]}✝{state[2]}☺"
 
-nodesToProcess =  Queue()
+def edgeToString(edge : Tuple[State,State]):
+    return f"\\{abs(edge[0][1]-edge[1][1])}✝{abs(edge[0][2]-edge[1][2])}☺/"
+
+nodesToProcess : "Queue[State]" =  Queue()
 nodesToProcess.put(start)
-visited = set()
-prev = dict()
+visited : Set[State] = set()
+prev : Dict[State,State] = dict()
 while not nodesToProcess.empty():
     node = nodesToProcess.get()
     if node in visited:
         continue
     visited.add(node)
-    for x in adjList[node]:
+    adjList[node] = []
+    for x in neighbours(node):
+        adjList[node].append(x)
         if x not in prev:
             prev[x] = node
         if x not in visited:
--- generategraph.py
+++ generategraph.py
@@ -3,6 +3,8 @@ import calculatepath as lib
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
+
+from calculatepath import edgeToString
 figure(num=None, figsize=(23, 10), dpi=80, facecolor='w', edgecolor='k')
 
 G = nx.Graph()
@@ -11,14 +13,22 @@ G.add_edges_from( {tuple(sorted((key,value))) for key,values in lib.adjList.item
 
 plt.subplot(111)
 plt.tight_layout(pad=5)
-nx.draw_kamada_kawai(
+pos = nx.kamada_kawai_layout(G)
+nx.draw(
     G, 
+    pos,
     with_labels=True, 
     font_weight='bold',
     font_size=20,
     edgecolors=["#FF0000" if state == lib.start or state == lib.end else '#00FF00' for state in list(G.nodes)],
     labels={state:lib.stateToString(state) for state in lib.adjList.keys()},
     node_color=["#00FF00" if state in lib.correct_path else "#00FFFF" for state in list(G.nodes)],
-    edge_color=["#00FF00" if edge in lib.correct_edges else "#000000" for edge in list(G.edges)])
+    edge_color=["#00FF00" if edge in lib.correct_edges else "#000000" for edge in list(G.edges)],
+    )
+nx.draw_networkx_edge_labels(
+    G,
+    pos,
+    edge_labels={x:edgeToString(x) for x in G.edges}
+)
 plt.savefig("Graph.png")
 #%%
